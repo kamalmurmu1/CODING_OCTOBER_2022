8e457c14f9e519bf3f87581a335d7db445dcb1a2
kamalmurmu1 <kamalmurmu420@gmail.com>
2022-10-01 20:24:10 +0530

    INIT

---
+++ CODIN_OCTOBER_2022/src/TemplateJava.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TemplateJava {
+    public static void solve()
+    {
+//        System.out.println("Hello Sir");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            solve();
+        }
+    }
+}
---
+++ CODIN_OCTOBER_2022/src/com/october_01_2022/KTTABLE.java
@@ -0,0 +1,36 @@
+package com.october_01_2022;
+
+import java.util.Scanner;
+
+public class KTTABLE {
+    static Scanner sc = new Scanner(System.in);
+    public static void solve()
+    {
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        int b[] = new int[n];
+        for(int i=0;i<n;i++) a[i] = sc.nextInt();
+        for(int i=0;i<n;i++) b[i] = sc.nextInt();
+
+        int res = 0 ,test=0;
+        for(int i=0;i<n;i++)
+        {
+            int test1 = a[i] - test;
+            if(test1 >= b[i])
+            {
+                res++;
+            }
+            test = a[i];
+        }
+        System.out.println(res);
+
+    }
+    public static void main(String[] args) throws java.lang.Exception{
+
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            solve();
+        }
+    }
+}
